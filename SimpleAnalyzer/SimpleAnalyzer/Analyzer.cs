8bb720fba59b0f01b0ab3bf154a495ea24c7b380
Valery Petrov <m08pvv@ya.ru>
2017-08-25 17:32:11 +0300

    Added AssemblyResolveHelper

--- SimpleAnalyzer/SimpleAnalyzer/Analyzer.cs
+++ SimpleAnalyzer/SimpleAnalyzer/Analyzer.cs
@@ -12,44 +12,59 @@ namespace SimpleAnalyzer
     public static class GloablOptions
     {
         public static bool Verbose { get; set; }
+        public static bool AssemblyResolverDebugOutput { get; set; }
+
+        public static void Set(Options options)
+        {
+            Verbose = options.Verbose;
+            AssemblyResolverDebugOutput = options.AssemblyResolverDebugOutput;
+        }
+    }
+
+    public class Options
+    {
+        [Option('d', "directory", Required = true, HelpText = "Directory for search with pattern *.cs")]
+        public string Directory { get; set; }
+
+        [Option('r', "recursive", Default = false, HelpText = "Option to search recursively in directory")]
+        public bool Recursive { get; set; }
+
+        [Option('v', "verbose", Default = false, HelpText = "Display debug output")]
+        public bool Verbose { get; set; }
+
+        [Option('a', "assmblyresolver", Default = false, HelpText = "Display assembly resolver debug output")]
+        public bool AssemblyResolverDebugOutput { get; set; }
     }
 
     public class Analyzer
     {
-        class Options
-        {
-            [Option('d', "directory", Required = true, HelpText = "Directory for search with pattern *.cs")]
-            public string Directory { get; set; }
-
-            [Option('r', "recursive", Default = false, HelpText = "Option to search recursively in directory")]
-            public bool Recursive { get; set; }
-
-            [Option('v', "verbose", Default = false, HelpText = "Display debug output")]
-            public bool Verbose { get; set; }
-        }
-
         private static void Main(string[] args)
         {
             var parsedOptions = (Parser.Default.ParseArguments<Options>(args) as Parsed<Options>)?.Value;
             if (parsedOptions == null)
                 return;
 
-            GloablOptions.Verbose = parsedOptions.Verbose;
+            GloablOptions.Set(parsedOptions);
 
             var analyzedFilesCount = 0;
 
-            foreach (var fileName in Directory.EnumerateFiles(parsedOptions.Directory, "*.cs", parsedOptions.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
+            using(AssemblyResolveHelper.CreateHelper())
             {
-                var result = AnalyzeCode(File.ReadAllText(fileName)).ToList();
-                ++analyzedFilesCount;
 
-                if (result.Any())
+                foreach (var fileName in Directory.EnumerateFiles(parsedOptions.Directory, "*.cs",
+                    parsedOptions.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
                 {
-                    foreach (var symbol in result)
+                    var result = AnalyzeCode(File.ReadAllText(fileName)).ToList();
+                    ++analyzedFilesCount;
+
+                    if (result.Any())
                     {
-                        Console.WriteLine($"In file {fileName} found potential issue for symbol {symbol}");
+                        foreach (var symbol in result)
+                        {
+                            Console.WriteLine($"In file {fileName} found potential issue for symbol {symbol}");
+                        }
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
                 }
             }
 
---
+++ SimpleAnalyzer/SimpleAnalyzer/AssemblyResolveHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace SimpleAnalyzer
+{
+    internal class AssemblyResolveHelper : IDisposable
+    {
+        public static AssemblyResolveHelper CreateHelper()
+        {
+            return new AssemblyResolveHelper();
+        }
+
+        private AssemblyResolveHelper()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += HelpTheFrameworkToResolveTheAssembly;
+        }
+
+        public void Dispose()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= HelpTheFrameworkToResolveTheAssembly;
+        }
+        private Assembly HelpTheFrameworkToResolveTheAssembly(object sender, ResolveEventArgs args)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{new AssemblyName(args.Name).Name}.dll");
+
+            if (!File.Exists(path))
+            {
+                if (GloablOptions.AssemblyResolverDebugOutput)
+                    Console.WriteLine($"Failed to load {args.Name} from {path}");
+                return null;
+            }
+
+            if (GloablOptions.AssemblyResolverDebugOutput)
+                Console.WriteLine($"Failed to load {args.Name}, loaded from {path}");
+
+            return Assembly.LoadFrom(path);
+        }
+    }
+}
